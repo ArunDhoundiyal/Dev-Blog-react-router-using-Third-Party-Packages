a93b7025dc2fa7a331a99e14844702e8e5425732
ArunDhoundiyal <arunprakash23797@gmail.com>
2024-03-01 14:38:01 +0000

    Routing using react-router Part 2 Using fetch()

---
+++ src/components/BlogsList/index.js
@@ -0,0 +1,44 @@
+import {Component} from 'react'
+import Loader from 'react-loader-spinner'
+
+import BlogItem from '../BlogItem'
+
+import './index.css'
+
+class BlogsList extends Component {
+  state = {blogsData: [], isLoading: true}
+
+  componentDidMount() {
+    this.getBlogsData()
+  }
+
+  getBlogsData = async () => {
+    const response = await fetch('https://apis.ccbp.in/blogs')
+    const data = await response.json()
+    const updateData = data.map(eachItem => ({
+      id: eachItem.id,
+      author: eachItem.author,
+      avatarUrl: eachItem.avatar_url,
+      imageUrl: eachItem.image_url,
+      title: eachItem.title,
+      topic: eachItem.topic,
+    }))
+    console.log(updateData)
+    this.setState({blogsData: updateData, isLoading: false})
+  }
+
+  render() {
+    const {blogsData, isLoading} = this.state
+    return (
+      <div className="blog-list-container">
+        {isLoading ? (
+          <Loader type="TailSpin" color="#00BFFF" height={50} width={50} />
+        ) : (
+          blogsData.map(item => <BlogItem blogData={item} key={item.id} />)
+        )}
+      </div>
+    )
+  }
+}
+
+export default BlogsList
